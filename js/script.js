d83509668040f0c6cdd8617c9abac296b7341319
Krystian Krupiński <krupinskikrystian22@gmail.com>
2023-06-07 12:25:03 +0200

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+{
+const calculateResult = (amount, currency) => {
 
-let EUR = 4.59;
-let USD = 4.19;
-let GBP = 5.22;
-let CHF = 4.66;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let result;
+    const EUR = 4.59;
+    const USD = 4.19;
+    const GBP = 5.22;
+    const CHF = 4.66;
 
     switch (currency) {
         case "EUR":
-            result = amount / EUR;
-            break;
+            return amount / EUR;
+
         case "USD":
-            result = amount / USD;
-            break;
+            return amount / USD;
+
         case "GBP":
-            result = amount / GBP;
-            break;
+            return amount / GBP;
+
         case "CHF":
-            result = amount / CHF;
-            break;
+            return amount / CHF;
+
     }
+};
+
+const updateResultText = (amount, result, currency) =>{
+    const resultElement = document.querySelector(".js-result");
     resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-});
+}
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+    const result = calculateResult(amount, currency)
+
+    updateResultText(amount, result, currency);
+
+};
+
+const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
+};
+    init();
+}
